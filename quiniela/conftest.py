e3b40e97ee2d22d50f45a6541b60b62d86a0002c
Rodrigo <rodrichavezm@gmail.com>
2021-03-27 13:24:01 -0600

    Initial commit

---
+++ quiniela/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from quiniela.users.models import User
+from quiniela.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
